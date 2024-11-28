c8b2d3a5a8bc9e88a2b24de7449db1e7640734af
Niclas <69795173+niclasclassen@users.noreply.github.com>
2024-11-28 18:40:13 +0100

    update utils.py

--- utils.py
+++ utils.py
@@ -4,11 +4,27 @@ def get_all_domains(path="data/amazon_reviews_categories.txt"):
     return domains
 
 
-def get_domain_reviews(domain, path="data/reviews"):
-    from datasets import load_from_disk
+def get_domain_reviews(domain, path="data/reviews/json"):
+    import pandas as pd
+    import json
 
-    file_path = f"{path}/{domain}_reviews/"
-    return load_from_disk(file_path)
+    file_path = f"{path}/{domain}_reviews"
+
+    # Read the JSONL file
+    reviews = []
+    with open(file_path, "r") as file:
+        for line in file:
+            reviews.append(json.loads(line))
+
+    # Convert to pandas DataFrame
+    df = pd.DataFrame(reviews)
+
+    return df
+
+    # from datasets import load_from_disk
+
+    # file_path = f"{path}/{domain}_reviews/"
+    # return load_from_disk(file_path)
 
 
 def get_domain_metadata(domain, path="data/metadata"):
